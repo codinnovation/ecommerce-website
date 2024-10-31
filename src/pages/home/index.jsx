a35ba31f3e6475f1fc48cc0337714e6fdb21dbeb
Kwabena Asumadu <codinnovations@gmail.com>
2024-10-31 11:25:20 +0000

    installed mui materials and designed first header

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Topbar from "./topbar";
+
+function HomePage() {
+  return (
+    <>
+      <div>
+        <Topbar />
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
